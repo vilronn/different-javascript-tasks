04e9d8e02f33decb983f655ad3e0d5d9ae54afcd
vilronn <102553486+vilronn@users.noreply.github.com>
2025-05-13 19:48:14 +0300

    Create 606_classy_classes.js

---
+++ codewars/part_II/606_classy_classes.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    get info() {
+      return `${this.name}'s age is ${this.age}`;
+    }
+  }
+  
+  let john = new Person('john', 34);
+  console.log(john.info); //john's age is 34
