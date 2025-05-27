10521a40af9e25c2d406f2ad0d394239c95a62e7
vilronn <102553486+vilronn@users.noreply.github.com>
2025-05-27 22:53:57 +0300

    Create 703_fun_with_trees_max_sum.js

---
+++ codewars/part_II/703_fun_with_trees_max_sum.js
@@ -0,0 +1,34 @@
+function maxRootToLeafSum(node) {
+  if (node === null) {
+    return 0;
+  }
+
+  if (node.left === null && node.right === null) {
+    return node.value;
+  }
+
+  const leftSum = maxRootToLeafSum(node.left);
+  const rightSum = maxRootToLeafSum(node.right);
+
+  return node.value + Math.max(leftSum, rightSum);
+}
+
+class TreeNode {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const tree = new TreeNode(
+  17,
+  new TreeNode(3, new TreeNode(2)),
+  new TreeNode(
+    -10,
+    new TreeNode(16),
+    new TreeNode(1, new TreeNode(13))
+  )
+);
+
+console.log(maxRootToLeafSum(tree));
