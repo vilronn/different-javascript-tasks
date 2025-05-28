e59efc4d86f84da5a33cc1ac2d1e4f9ee301955a
vilronn <102553486+vilronn@users.noreply.github.com>
2025-05-28 22:43:11 +0300

    object 704_smart_sum.js

--- codewars/part_II/704_smart_sum.js
+++ codewars/part_II/704_smart_sum.js
@@ -6,13 +6,22 @@ function smartSum(...args) {
       sum += arg;
     } else if (Array.isArray(arg)) {
       sum += smartSum(...arg);
+    } else if (arg && typeof arg === 'object') {
+      sum += smartSum(...Object.values(arg));
     }
   }
+
   return sum;
 }
+
 
 console.log(smartSum(1, 2, 3)); //6
 console.log(smartSum(1, 2, [3, 4])); //10
 console.log(smartSum(1, 2, [[3, 4], 5], 6)); //21
 console.log(smartSum()); //0
+console.log(smartSum(1, 2, [3, 4], { a: 5, b: 6 })); //21
+console.log(smartSum({ a: [1, 2], b: { c: 3 } }, 4)); //10
+console.log(smartSum({})); //0
+console.log(smartSum(null, 1, undefined, '2', true, [3])); //4
+
 
