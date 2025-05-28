62cef64fef7643bd52bdfaf12079899e5f13064c
vilronn <102553486+vilronn@users.noreply.github.com>
2025-05-28 22:39:28 +0300

    tests 704_smart_sum.js

--- codewars/part_II/704_smart_sum.js
+++ codewars/part_II/704_smart_sum.js
@@ -10,3 +10,9 @@ function smartSum(...args) {
   }
   return sum;
 }
+
+console.log(smartSum(1, 2, 3)); //6
+console.log(smartSum(1, 2, [3, 4])); //10
+console.log(smartSum(1, 2, [[3, 4], 5], 6)); //21
+console.log(smartSum()); //0
+
